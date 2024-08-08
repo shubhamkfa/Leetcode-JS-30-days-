fbf82a61110a793c3a63cd5a4bfbf8130799b461
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-08 20:19:37 +0530

    Add files via upload
    Day 1st

---
+++ 2667. Create Hello World Function.js
@@ -0,0 +1,32 @@
+// /**
+//  * @return {Function}
+//  */
+var createHelloWorld = function() {
+    
+    return function () {
+        return "Hello World"
+    }
+    
+}
+
+createHelloWorld();
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+
+
+//--------------------------------------------------------------MDN----------------
+//Closure
+
+function init() {
+    var name = "Mozilla"; // name is a local variable created by init
+    function displayName() {
+      // displayName() is the inner function, that forms the closure
+      console.log(name); // use variable declared in the parent function
+    }
+    displayName();
+  }
+  init();
